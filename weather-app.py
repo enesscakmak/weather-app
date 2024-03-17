f32c6d36e8e4b403f58cd45e591aedcb86ad8511
Enes Çakmak <cakmakkeness@gmail.com>
2024-03-17 17:46:21 +0300

    feat: created form and expanded index

--- weather-app.py
+++ weather-app.py
@@ -1,30 +1,47 @@
 import requests, json
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired
+
 from config import api_key
+from flask import Flask, render_template, redirect, url_for, request, flash
+from wtforms import StringField, SubmitField
 
 base_url = "https://api.openweathermap.org/data/2.5/weather?"
 
-city_name = input("Enter city: ")
+app = Flask(__name__)
 
-complete_url = base_url + "appid=" + api_key + "&q=" + city_name
-response = requests.get(complete_url)
 
-x = response.json()
+class WeatherForm(FlaskForm):
+    city = StringField('City', validators=[DataRequired()])
+    submit = SubmitField('Search')
 
-if x["cod"] != "404":
-    y = x["main"]
-    current_temperature = y["temp"]
-    current_pressure = y["pressure"]
-    current_humidity = y["humidity"]
-    z = x["weather"]
-    weather_description = z[0]["description"]
-    print(" Temperature (in kelvin unit) = " +
-          str(current_temperature) +
-          "\n atmospheric pressure (in hPa unit) = " +
-          str(current_pressure) +
-          "\n humidity (in percentage) = " +
-          str(current_humidity) +
-          "\n description = " +
-          str(weather_description))
-else:
-    print(" City Not Found ")
 
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/<string:city>', methods=['GET', 'POST'])
+def index():
+    form = WeatherForm()
+    if form.validate_on_submit():
+        city = form.city.data
+        complete_url = base_url + "appid=" + api_key + "&q=" + city
+        response = requests.get(complete_url)
+        x = response.json()
+        if x["cod"] != "404":
+            main = x["main"]
+            current_temperature = main["temp"]
+            current_pressure = main["pressure"]
+            current_humidity = main["humidity"]
+            weather = x["weather"]
+            country = x["country"]
+            name = x["name"]
+            weather_description = weather[0]["description"]
+            return render_template('index.html', city=city, country=country, name=name, current_temperature=current_temperature,
+                                   current_pressure=current_pressure, current_humidity=current_humidity,
+                                   weather_description=weather_description)
+        else:
+            flash("City Not Found")
+            return redirect(url_for('index'))
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
